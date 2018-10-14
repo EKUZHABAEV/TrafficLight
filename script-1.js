e05412f8955a50370a7119f46ec27cc7baa9ff98
EKUZHABAEV <ekuzhabaev@tsconsulting.ru>
2018-10-14 20:37:03 +0400

    little change

--- script-1.js
+++ script-1.js
@@ -1,31 +1,21 @@
-//js file
-function Traffic(){
 var dT_green = 25;
 var dT_red = 134;
 
-var f = function TimeFlow(var dT_green, var dT_red)
+function Traffic()
 {
-		var i=0;
-		i++;
-		if(dT_green=0)
-		{
-			dT_red=dT_red-i;
-			document.getElementById("demo").innerHTML = dT_red;
-		}
-		else 
-		{
-			dT_green=dT_green-i;
-			document.getElementById("demo").innerHTML = dT_green;
-			
-		}
-		if((dT_red>0) and (dT_green>0))
-		{
-		var t = setTimeout(TimeFlow, 1000);
-		}
-		else {var t = setTimeout(Traffic, 1000);}
-		
-}
 
+if(dT_green != 0)
+{
+
+dT_green--;
+document.getElementById("demo").innerHTML = dT_green;
+
+}
+else {
+	dT_red--;
+	document.getElementById("demo").innerHTML = dT_red;
 }
 
 
+var t = setTimeout(Traffic, 1000);
+}
